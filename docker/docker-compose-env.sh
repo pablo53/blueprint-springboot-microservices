aba3e7f1f942f8f5ebc59254cc8edea3f5ea7b55
Paweł Ryszawa <pablo53@poczta.onet.eu>
2020-09-05 03:00:56 +0200

    PostgreSQL 12 (via Docker)

--- docker/docker-compose-env.sh
+++ docker/docker-compose-env.sh
@@ -1,4 +1,6 @@
 export CONSUL_DATA_DIR=$(pwd)/volume/consul/data
 export CONSUL_CONFIG_DIR=$(pwd)/volume/consul/config
+export POSTGRES_DATA_DIR=$(pwd)/volume/postgres/data
 mkdir -p "${CONSUL_DATA_DIR}"
 mkdir -p "${CONSUL_CONFIG_DIR}"
+mkdir -p "${POSTGRES_DATA_DIR}"
