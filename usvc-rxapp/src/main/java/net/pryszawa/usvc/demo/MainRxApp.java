2eba48cde479daaf4d84ba68eeda510ccbe4eee9
Paweł Ryszawa <pablo53@poczta.onet.eu>
2020-09-15 05:19:08 +0200

    WebFlux app

---
+++ usvc-rxapp/src/main/java/net/pryszawa/usvc/demo/MainRxApp.java
@@ -0,0 +1,13 @@
+package net.pryszawa.usvc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainRxApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainRxApp.class, args);
+    }
+
+}
