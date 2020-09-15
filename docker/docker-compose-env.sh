2de52fb74806127c0aa63c44a81330adf2eb8bfd
Paweł Ryszawa <pablo53@poczta.onet.eu>
2020-09-15 05:02:45 +0200

    MongoDB and minor fixes

--- docker/docker-compose-env.sh
+++ docker/docker-compose-env.sh
@@ -1,6 +1,8 @@
 export CONSUL_DATA_DIR=$(pwd)/volume/consul/data
 export CONSUL_CONFIG_DIR=$(pwd)/volume/consul/config
 export POSTGRES_DATA_DIR=$(pwd)/volume/postgres/data
+export MONGO_DATA_DIR=$(pwd)/volume/mongo/data
 mkdir -p "${CONSUL_DATA_DIR}"
 mkdir -p "${CONSUL_CONFIG_DIR}"
 mkdir -p "${POSTGRES_DATA_DIR}"
+mkdir -p "${MONGO_DATA_DIR}"
