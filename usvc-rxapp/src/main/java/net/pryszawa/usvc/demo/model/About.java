c13b63efb845f8547eedf6f0c621c6987662cb3e
Paweł Ryszawa <pablo53@poczta.onet.eu>
2020-09-15 06:29:52 +0200

    Reactive MongoDB, model

---
+++ usvc-rxapp/src/main/java/net/pryszawa/usvc/demo/model/About.java
@@ -0,0 +1,18 @@
+package net.pryszawa.usvc.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Getter
+@Setter
+public class About {
+
+    @Id
+    private String id;
+
+    private String desc;
+
+}
