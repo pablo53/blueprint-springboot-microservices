13f34647ed0c655d6ec628919de9e2b460a19a7f
Paweł Ryszawa <pablo53@poczta.onet.eu>
2020-09-16 02:50:10 +0200

    Elasticsearch + Kibana (Docker) and minor improvement

--- docker/docker-compose-env.sh
+++ docker/docker-compose-env.sh
@@ -3,9 +3,13 @@ export CONSUL_CONFIG_DIR=$(pwd)/volume/consul/config
 export POSTGRES_DATA_DIR=$(pwd)/volume/postgres/data
 export MONGO_DATA_DIR=$(pwd)/volume/mongo/data
 export JAEGER_DATA_DIR=$(pwd)/volume/jaeger/data
+export ELK_DATA_DIR=$(pwd)/volume/elk/data
+export KIBANA_CONFIG_DIR=$(pwd)/volume/kibana/config
 
 mkdir -p "${CONSUL_DATA_DIR}"
 mkdir -p "${CONSUL_CONFIG_DIR}"
 mkdir -p "${POSTGRES_DATA_DIR}"
 mkdir -p "${MONGO_DATA_DIR}"
 mkdir -p "${JAEGER_DATA_DIR}"
+mkdir -p "${ELK_DATA_DIR}"
+mkdir -p "${KIBANA_CONFIG_DIR}"
