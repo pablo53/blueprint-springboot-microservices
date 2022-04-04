5028f4b80acbdddeeb8e40db8bbf637b8708748f
Paweł Ryszawa <pablo53@poczta.onet.eu>
2022-04-04 17:29:34 +0200

    opening actuator - fix after OAuth2 introduction

--- usvc-app2/src/main/kotlin/net/pryszawa/usvc/demo/config/SecurityConfig.kt
+++ usvc-app2/src/main/kotlin/net/pryszawa/usvc/demo/config/SecurityConfig.kt
@@ -12,7 +12,7 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity) {
         http.authorizeHttpRequests { authz -> authz
-            .antMatchers(HttpMethod.GET, "/favicon.ico", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
+            .antMatchers(HttpMethod.GET, "/actuator", "/actuator/**", "/favicon.ico", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
             .anyRequest().hasAuthority("SCOPE_full-scope") // SCOPE_ prefix is necessary when checking scopes
         }.oauth2ResourceServer { oauth2 -> oauth2
             .jwt()
