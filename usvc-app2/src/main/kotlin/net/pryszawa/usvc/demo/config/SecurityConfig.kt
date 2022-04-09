771151eb39938b25cc536b169c6c6d736d2f605e
Paweł Ryszawa <pablo53@poczta.onet.eu>
2022-04-09 11:22:51 +0200

    CORS in SpringBoot

--- usvc-app2/src/main/kotlin/net/pryszawa/usvc/demo/config/SecurityConfig.kt
+++ usvc-app2/src/main/kotlin/net/pryszawa/usvc/demo/config/SecurityConfig.kt
@@ -11,12 +11,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 class SecurityConfig : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity) {
-        http.authorizeHttpRequests { authz -> authz
-            .antMatchers(HttpMethod.GET, "/actuator", "/actuator/**", "/favicon.ico", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
-            .anyRequest().hasAuthority("SCOPE_full-scope") // SCOPE_ prefix is necessary when checking scopes
-        }.oauth2ResourceServer { oauth2 -> oauth2
-            .jwt()
-        }
+        http.cors().and()
+            .authorizeHttpRequests { authz -> authz
+                .antMatchers(HttpMethod.GET, "/actuator", "/actuator/**", "/favicon.ico", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
+                .anyRequest().hasAuthority("SCOPE_full-scope") // SCOPE_ prefix is necessary when checking scopes
+            }.oauth2ResourceServer { oauth2 -> oauth2
+                .jwt()
+            }
     }
 
 }
--- usvc-app2/src/main/kotlin/net/pryszawa/usvc/demo/controller/ProxyController.kt
+++ usvc-app2/src/main/kotlin/net/pryszawa/usvc/demo/controller/ProxyController.kt
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import net.pryszawa.usvc.demo.service.ProxyService
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/proxy")
+@CrossOrigin(origins = [ "*" ])
 class ProxyController(
     private val proxyService: ProxyService,
 ) {
